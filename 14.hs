1750eb5ab8fcf4dc7836d8b13ef4a53d804067ca
Simon Michael <simon@joyful.com>
2018-12-14 23:46:44 +0000

    14 cleanups.. attempts.. possibly right but too slow

--- 14.hs
+++ 14.hs
@@ -1,6 +1,20 @@
 #!/usr/bin/env stack
 {- stack --resolver=nightly-2018-12-12 script --compile
-   --package "ansi-terminal terminal-size containers here megaparsec monad-loops mtl pretty-show safe scanf split time timeit vector"
+   --package ansi-terminal
+   --package call-stack
+   --package containers
+   --package here
+   --package megaparsec
+   --package monad-loops
+   --package mtl
+   --package pretty-show
+   --package safe
+   --package scanf
+   --package split
+   --package terminal-size
+   --package time
+   --package timeit
+   --package vector
 -}
 -- relude 
 -- {-# Language NoImplicitPrelude #-}
@@ -78,7 +92,7 @@ forceAllElementsWHNF = foldr seq ()
 -- assert that prints to stdout in normal output sequence
 assert_ :: (HasCallStack, MonadIO m) => Bool -> m ()
 assert_ True  = return ()
-assert_ False = liftIO $ putStrLn " assertion failed" >> exitFailure
+assert_ False = liftIO $ putStrLn ": wrong" >> exitFailure
 
 -- timeIt :: MonadIO m => m a -> m a
 -- Wrap a MonadIO computation so that it prints out the execution time.
@@ -130,7 +144,7 @@ data W = W { -- world
 
 parse :: String -> W
 parse s =
-  W{wtime   = 0
+  W{wtime   = 1
    ,wscores = map toscore s
    ,welf1   = 0
    ,welf2   = 1
@@ -150,29 +164,52 @@ main = do
   -- -- let [f] = take 1 $ args ++ drop (length args) defargs
   -- -- input <- parse <$> readFile f
 
-  ss <- fmap (concatMap show) $ nextScores 9 10 return $ parse "37"
-  putStr ss >> assert_ (ss == "5158916779") >> putStrLn " ok"
+  -- ss <- fmap (concatMap show) $ nextScores 9 10 return $ parse "37"
+  -- putStr ss >> assert_ (ss == "5158916779") >> putStrLn ": ok"
 
-  ss <- fmap (concatMap show) $ nextScores 5 10 return $ parse "37"
-  putStr ss >> assert_ (ss == "0124515891") >> putStrLn " ok"
+  -- ss <- fmap (concatMap show) $ nextScores 5 10 return $ parse "37"
+  -- putStr ss >> assert_ (ss == "0124515891") >> putStrLn ": ok"
 
-  ss <- fmap (concatMap show) $ nextScores 18 10 return $ parse "37"
-  putStr ss >> assert_ (ss == "9251071085") >> putStrLn " ok"
+  -- ss <- fmap (concatMap show) $ nextScores 18 10 return $ parse "37"
+  -- putStr ss >> assert_ (ss == "9251071085") >> putStrLn ": ok"
 
-  ss <- fmap (concatMap show) $ nextScores 2018 10 return $ parse "37"
-  putStr ss >> assert_ (ss == "5941429882") >> putStrLn " ok"
+  -- ss <- fmap (concatMap show) $ nextScores 2018 10 return $ parse "37"
+  -- putStr ss >> assert_ (ss == "5941429882") >> putStrLn ": ok"
 
-  let input = "633601"
-      wrong = ["9714214618"]
-  ss <- fmap (concatMap show) $ nextScores (length input) 10 printworld $ parse input
-  putStr ss >> assert_ (not $ ss `elem` wrong) >> putStrLn " ok"
+  
+  -- let ontheboard = "3710"
+  --     input = "633601"
+  --     wrong = ["9714214618", "1034973697", "3497369736"]   -- rejected answers (also 9,7,1,4,2,1,4,6,1,8)
+  -- ss <- fmap (concatMap show) $ nextScores (length input) 10 printworld $ parse (ontheboard ++ input)
+  -- -- putStr ss >> assert_ (not $ ss `elem` wrong) >> putStrLn ": ok"
+  -- putStrLn ss
+
+  let (usage,defargs) = ("Usage: ./14 [STARTAFTERNUM]", ["10000"])
+  args <- getArgs
+  let [after'] = take 1 $ args ++ drop (length args) defargs
+      after = read after'
+  -- when (null args) $ putStrLn usage >> exitSuccess
+  -- let [f] = take 1 $ args ++ drop (length args) defargs
+  -- input <- parse <$> readFile f
+
+  hSetBuffering stdout NoBuffering
+  ss <- fmap (concatMap show) $ nextScores after 10 printdots $ parse "37"
+  putStrLn ss
+
+  -- ss <- fmap (concatMap show) $ nextScores 633601 10 printstats $ parse "37"
+  -- putStrLn ss
+
+
 
 -- part 1 world update function
 update1 :: W -> IO W
 update1 w@W{..} = do
   let
     (cur1,cur2) = (wscores!!welf1, wscores!!welf2)
-    wscores' = wscores ++ (map toscore $ show $ sum [cur1,cur2])
+    (digit1,digit2) = sum [cur1,cur2] `divMod` 10
+    wscores' = (digit2:) $
+               (if digit1 > 0 then (digit1:) else id) $
+               wscores
     welf1' = (welf1 + cur1 + 1) `mod` length wscores'
     welf2' = (welf2 + cur2 + 1) `mod` length wscores'
   return w{
@@ -185,7 +222,8 @@ update1 w@W{..} = do
 -- | after the first m scores in the given World, get the next n scores
 nextScores :: Int -> Int -> (W -> IO W) -> W -> IO [S]
 nextScores afterm nextn display w = do
-  w' <- iterateUntilM (recipes (afterm + nextn)) (update1 >=> display) <=< display $ w
+  (t,w') <- timeItT $ iterateUntilM (recipes (afterm + nextn)) (update1 >=> display) <=< display $ w
+  printf "%.3fs to generate %d scores (%.0f scores/s)\n" t afterm (fromIntegral afterm / t)
   return $ take nextn $ drop afterm $ wscores w'
 
 -- display functions, these return the unmodified World for easier chaining
@@ -201,3 +239,74 @@ printworld w@W{..} = do
       | i==welf2             = "["++show d++"]"
       | otherwise            = " "++show d++" "
 
+printstats w@W{..} = do
+  when (wtime `mod` 1000 == 0) $ printf "%4d (%4d recipes)\n" wtime (length wscores)
+  return w
+
+printdots w@W{..} = do
+  when (wtime `mod` 1000 == 0) (putStr ".")
+  return w
+
+
+
+
+{-
+                             <----input----->
+   0 (   8 recipes):  (3)[7] 6  3  3  6  0  1 
+   1 (  10 recipes):   3  7  6  3 (3) 6  0  1  1 [0]
+   2 (  11 recipes):   3  7  6  3  3  6  0  1 (1) 0 [3]
+   3 (  12 recipes):   3  7 [6] 3  3  6  0  1  1  0 (3) 4 
+   4 (  13 recipes):   3 (7) 6  3  3  6  0  1  1 [0] 3  4  9 
+   5 (  14 recipes):   3  7  6  3  3  6  0  1  1 (0)[3] 4  9  7 
+   6 (  15 recipes):   3  7  6  3  3  6  0  1  1  0 (3) 4  9  7 [3]
+   7 (  16 recipes):   3  7 [6] 3  3  6  0  1  1  0  3  4  9  7 (3) 6 
+   8 (  17 recipes):   3 (7) 6  3  3  6  0  1  1 [0] 3  4  9  7  3  6  9 
+   9 (  18 recipes):   3  7  6  3  3  6  0  1  1 (0)[3] 4  9  7  3  6  9  7 
+                                               <---------output?---------->
+1034973697: wrong
+
+   0 (   8 recipes):  (3)[7] 6  3  3  6  0  1 
+   1 (  10 recipes):   3  7  6  3 (3) 6  0  1  1 [0]
+   2 (  11 recipes):   3  7  6  3  3  6  0  1 (1) 0 [3]
+   3 (  12 recipes):   3  7 [6] 3  3  6  0  1  1  0 (3) 4 
+   4 (  13 recipes):   3 (7) 6  3  3  6  0  1  1 [0] 3  4  9 
+   5 (  14 recipes):   3  7  6  3  3  6  0  1  1 (0)[3] 4  9  7 
+   6 (  15 recipes):   3  7  6  3  3  6  0  1  1  0 (3) 4  9  7 [3]
+   7 (  16 recipes):   3  7 [6] 3  3  6  0  1  1  0  3  4  9  7 (3) 6 
+   8 (  17 recipes):   3 (7) 6  3  3  6  0  1  1 [0] 3  4  9  7  3  6  9 
+   9 (  18 recipes):   3  7  6  3  3  6  0  1  1 (0)[3] 4  9  7  3  6  9  7 
+  10 (  19 recipes):   3  7  6  3  3  6  0  1  1  0 (3) 4  9  7 [3] 6  9  7  3 
+  11 (  20 recipes):   3  7  6  3  3  6  0  1  1  0  3  4  9  7 (3) 6  9  7 [3] 6 
+                                                     <---------output?---------->
+3497369736: wrong
+
+   0 (  10 recipes):  (3)[7] 3  7  6  3  3  6  0  1 
+   1 (  12 recipes):   3  7  3  7 (6) 3  3  6  0 [1] 1  0 
+   2 (  13 recipes):   3  7  3  7  6  3  3  6  0  1  1 (0] 7 
+   3 (  14 recipes):   3  7  3  7  6  3  3  6  0  1  1  0 (7] 0 
+   4 (  16 recipes):   3  7  3  7 (6] 3  3  6  0  1  1  0  7  0  1  4 
+   5 (  18 recipes):   3  7  3  7  6  3  3  6  0  1  1 (0] 7  0  1  4  1  2 
+                                 <--mistake
+0110701412: WRONG
+
+   0 (   8 recipes):  (3)[7] 6  3  3  6  0  1 
+   1 (  10 recipes):   3  7  6  3 (3) 6  0  1  1 [0]
+   2 (  11 recipes):   3  7  6  3  3  6  0  1 (1) 0 [3]
+   3 (  12 recipes):   3  7 [6] 3  3  6  0  1  1  0 (3) 4 
+   4 (  13 recipes):   3 (7) 6  3  3  6  0  1  1 [0] 3  4  9 
+   5 (  14 recipes):   3  7  6  3  3  6  0  1  1 (0)[3] 4  9  7 
+   6 (  15 recipes):   3  7  6  3  3  6  0  1  1  0 (3) 4  9  7 [3]
+   7 (  16 recipes):   3  7 [6] 3  3  6  0  1  1  0  3  4  9  7 (3) 6 
+                                         <---------output?---------->
+0110349736: WRONG WRONG WRONG
+
+   1 (  10 recipes):  (3)[7] 1  0  6  3  3  6  0  1 
+   2 (  12 recipes):   3  7  1  0 (6) 3  3  6  0 [1] 1  0 
+   3 (  13 recipes):   3  7  1  0  6  3  3  6  0  1  1 (0] 7 
+   4 (  14 recipes):   3  7  1  0  6  3  3  6  0  1  1  0 (7] 0 
+   5 (  16 recipes):   3  7  1  0 (6] 3  3  6  0  1  1  0  7  0  1  4 
+                                         <---------output?---------->
+
+3601107014: no
+
+-}
