29f4269847c21caf7900630b783d27b5fa9f5e13
Simon Michael <simon@joyful.com>
2018-12-17 00:16:43 +0000

    16 done

--- 16.hs
+++ 16.hs
@@ -1,7 +1,6 @@
 #!/usr/bin/env stack
 {- stack --resolver=nightly-2018-12-12 script --compile
    --package ansi-terminal
-   --package astar
    --package call-stack
    --package containers
    --package here
@@ -17,7 +16,8 @@
    --package timeit
    --package vector
 -}
--- relude 
+   --package astar
+   --package relude 
 -- {-# Language NoImplicitPrelude #-}
 {-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE NamedFieldPuns #-}
@@ -32,6 +32,8 @@ import Control.Monad.Loops
 import Control.Monad.State
 import Debug.Trace
 import qualified Data.Array as A
+import Data.Array (Array,array,listArray,(!),(//))
+import Data.Bits
 import Data.Bifunctor
 import Data.CallStack
 import Data.Char
@@ -39,7 +41,7 @@ import Data.Either
 import Data.Foldable
 import Data.Function
 import Data.Functor
-import Data.Graph.AStar
+-- import Data.Graph.AStar
 import qualified Data.HashSet  as H
 import Data.IntMap (IntMap)
 import qualified Data.IntMap.Strict as IntMap
@@ -76,6 +78,10 @@ ltrace :: Show a => String -> a -> a
 ltrace msg x =
   trace (msg++": "++show x)
   x
+
+-- | Trace (print to stderr) a showable value using a custom show function.
+traceWith :: (a -> String) -> a -> a
+traceWith f a = trace (f a) a
 
 iterateN :: Int -> (a -> a) -> (a -> a)
 iterateN n f = (!! n) . iterate f   -- n must be positive
@@ -120,6 +126,9 @@ seqIndexL needle haystack =
 -- examples
 
 e1 = [here|
+Before: [3, 2, 1, 1]
+9 2 1 2
+After:  [3, 2, 2, 1]
 |]
 
 e2 = [here|
@@ -139,196 +148,181 @@ e6 = [here|
 
 -- data
 
-data W = W { -- simulation world
-   wtime    :: Ticks
-  ,wend     :: Maybe String  --  reason for ending
-} deriving (Eq,Show)
-type Ticks = Int -- simulation time, 0..
-type Seconds = Double  -- real world time
-type Delay = Seconds  -- a length of time to pause; negative means wait for input
-type X = Int
-type Y = Int
-type P = (X,Y)
+-- type Ticks = Int -- simulation time, 0..
+-- type Seconds = Double  -- real world time
+-- type Delay = Seconds  -- a length of time to pause; negative means wait for input
+-- type X = Int
+-- type Y = Int
+-- type P = (X,Y)
+-- data W = W { -- simulation world
+--    wtime    :: Ticks
+--   ,wend     :: Maybe String  --  reason for ending
+--   ,wregs    :: M
+-- } deriving (Eq,Show)
 
-parse :: HasCallStack => String -> W
-parse s =
+type RegisterNum = Int  -- 0..3
+type M = Array RegisterNum Int  -- machine (registers) state
+
+data I = I {  -- instruction
+   iop  :: Opcode
+  ,iia  :: Int        -- input a
+  ,iib  :: Int        -- input b
+  ,ioc  :: RegisterNum -- output c
+} deriving (Eq,Show)
+
+data U = U {  -- unknown instruction
+   un  :: OpcodeNum
+  ,ua  :: Int        -- input a
+  ,ub  :: Int        -- input b
+  ,uc  :: RegisterNum -- output c
+} deriving (Eq,Show)
+
+type OpcodeNum = Int  -- 0..15, corresponds to Opcodes but we don't know which
+
+data Opcode =
+    Addr
+  | Addi
+  | Mulr
+  | Muli
+  | Banr
+  | Bani
+  | Borr
+  | Bori
+  | Setr
+  | Seti
+  | Gtir
+  | Gtri
+  | Gtrr
+  | Eqir
+  | Eqri
+  | Eqrr
+  deriving (Show,Eq,Enum,Ord,Bounded)
+
+data E = E { -- example
+   eregs1 :: M
+  ,einstr :: U
+  ,eregs2 :: M
+} deriving (Show)
+
+-- Before: [3, 2, 1, 1]
+-- 9 2 1 2
+-- After:  [3, 2, 2, 1]
+-- 
+parseexample :: HasCallStack => String -> E
+parseexample s =
   let
-    ls = lines s
-    h  = length ls
-    w  = length $ head ls
+    l1:l2:l3:_ = lines s
+    rs1 = read $ drop 8 l1
+    [n,a,b,c] = map read $ words l2
+    rs2 = read $ drop 8 l3
   in
-    W{wtime   = 0
-     ,wend    = Nothing
+    E{eregs1 = listArray (0,3) rs1
+     ,einstr = U n a b c
+     ,eregs2 = listArray (0,3) rs2
     }
+
+parseunknowninstruction s = U n a b c where [n,a,b,c] = map read $ words s
+
+parseinput :: HasCallStack => String -> ([E],[U])
+parseinput s =
+  let
+    [e,u] = splitOn "\n\n\n\n" s
+    es    = map parseexample $ map unlines $ chunksOf 4 $ lines e
+    us    = map parseunknowninstruction $ lines u
+  in
+    (es,us)
+
+exec :: I -> M -> M
+exec (I Addr a b c) m = m//[(c, m!a+m!b)]                     -- (add register) stores into register C the result of adding register A and register B.
+exec (I Addi a b c) m = m//[(c, m!a+b)]                       -- (add immediate) stores into register C the result of adding register A and value B.
+exec (I Mulr a b c) m = m//[(c, m!a*m!b)]                     -- (multiply register) stores into register C the result of multiplying register A and register B.
+exec (I Muli a b c) m = m//[(c, m!a*b)]                       -- (multiply immediate) stores into register C the result of multiplying register A and value B.
+exec (I Banr a b c) m = m//[(c, (m!a) .&. (m!b))]             -- (bitwise AND register) stores into register C the result of the bitwise AND of register A and register B.
+exec (I Bani a b c) m = m//[(c, (m!a) .&. b)]                 -- (bitwise AND immediate) stores into register C the result of the bitwise AND of register A and value B.
+exec (I Borr a b c) m = m//[(c, (m!a) .|. (m!b))]             -- (bitwise OR register) stores into register C the result of the bitwise OR of register A and register B.
+exec (I Bori a b c) m = m//[(c, (m!a) .|. b)]                 -- (bitwise OR immediate) stores into register C the result of the bitwise OR of register A and value B.
+exec (I Setr a _ c) m = m//[(c, (m!a))]                       -- (set register) copies the contents of register A into register C. (Input B is ignored.)
+exec (I Seti a _ c) m = m//[(c, a)]                           -- (set immediate) stores value A into register C. (Input B is ignored.)
+exec (I Gtir a b c) m = m//[(c, if a > m!b then 1 else 0)]    -- (greater-than immediate/register) sets register C to 1 if value A is greater than register B. Otherwise, register C is set to 0.
+exec (I Gtri a b c) m = m//[(c, if m!a > b then 1 else 0)]    -- (greater-than register/immediate) sets register C to 1 if register A is greater than value B. Otherwise, register C is set to 0.
+exec (I Gtrr a b c) m = m//[(c, if m!a > m!b then 1 else 0)]  -- (greater-than register/register) sets register C to 1 if register A is greater than register B. Otherwise, register C is set to 0.
+exec (I Eqir a b c) m = m//[(c, if a == m!b then 1 else 0)]   -- (equal immediate/register) sets register C to 1 if value A is equal to register B. Otherwise, register C is set to 0.
+exec (I Eqri a b c) m = m//[(c, if m!a == b then 1 else 0)]   -- (equal register/immediate) sets register C to 1 if register A is equal to value B. Otherwise, register C is set to 0.
+exec (I Eqrr a b c) m = m//[(c, if m!a == m!b then 1 else 0)] -- (equal register/register) sets register C to 1 if register A is equal to register B. Otherwise, register C is set to 0.
+
+-- which opcodes have a behaviour matching this example ?
+opcodesbehavinglike :: E -> [Opcode]
+opcodesbehavinglike E{..} =
+  [o | o <- [minBound..maxBound], exec (einstr `withopcode` o) eregs1 == eregs2]
+    
+withopcode :: U -> Opcode -> I
+withopcode (U _ a b c) o = I o a b c
+
+-- for each opcode number used in these examples, based on their behaviour
+-- in these examples, which opcodes do they possibly correspond to ?
+candidatesbynum :: [E] -> [(OpcodeNum, [Opcode])]
+candidatesbynum examples = opsbynum
+  where
+    eopcodenum = un.einstr
+    esbynum = [ (eopcodenum $ head es, es)
+              | es <- groupBy ((==) `on` eopcodenum) $ sortOn eopcodenum examples
+              ]
+    opsbynum =
+      [(n,opssatisfyingall)
+      | (n, es) <- esbynum
+      , let opsforeachex = [opcodesbehavinglike e | e <- es]
+      , let allops = nub $ sort $ concat opsforeachex
+      , let opssatisfyingall = [o | o <- allops, (o `elem`) `all` opsforeachex]
+      ]
+
+-- given a set of examples of behaviour of unknown numbered opcodes, and knowing the
+-- the opcodes that exist and their behaviour, match up as many as possible
+-- of the opcode numbers and opcodes.
+-- Returns the identified opcodes and any remaining opcode numbers & candidate opcodes
+-- that could not be resolved further.
+identifyopcodes :: [E] -> ([(OpcodeNum,Opcode)], [(OpcodeNum,[Opcode])])
+identifyopcodes examples = (sort identified, sort remaining)
+  where
+    (identified, remaining) = identifyopcodes' ([], candidatesbynum examples)
+
+    -- identify opcode numbers with only one possible opcode candidate,
+    -- then remove that opcode from the candidates and try to identify
+    -- the remaining opcode numbers from the remaining candidates,
+    -- recursively until we can go no farther.
+    identifyopcodes' :: ([(OpcodeNum,Opcode)],[(OpcodeNum,[Opcode])]) -> ([(OpcodeNum,Opcode)], [(OpcodeNum,[Opcode])])
+    identifyopcodes' (identified, candidates) =
+      let
+        identifiedopcodes = map snd identified
+        candidatesrefined = nub $ sort [(n, os') | (n,os) <- candidates, let os' = os \\ identifiedopcodes, not $ null os']
+        (surematches, candidatesremaining) = partition ((==1).length.snd) candidatesrefined
+        surematchespairs = [(n,o) | (n,[o]) <- surematches]
+        identified' = identified ++ surematchespairs
+      in
+        if null candidatesremaining || null surematches
+        then (identified', candidatesremaining)
+        else identifyopcodes' (identified', candidatesremaining)
 
 -- main
 
 main :: HasCallStack => IO ()
 main = do
-  -- args <- getArgs
-  -- progname <- getProgName
-  -- let (usage, defargs) = ("Usage: "++progname++" [INPUTFILE]", [progname++".in"])
-  -- let [f] = take 1 $ args ++ drop (length args) defargs
-  -- input <- readFile f
-  let input = e1
+  args <- getArgs
+  let progname = "16"
+  let (usage, defargs) = ("Usage: "++progname++" [INPUTFILE]", [progname++".in"])
+  let [f] = take 1 $ args ++ drop (length args) defargs
+  input <- readFile f
 
   -- part 1
-  (t,w) <- timeItT $ bracket_ initterm resetterm $
-    iterateUntilM (isJust.wend)
-      (update >=> displayworld (-1)) <=< displayworld (-1)
-      $ parse input
-  printsummary w t
+  let (examples, instructions) = parseinput input
+  pp $ length $ filter ((>=3).length) $ map opcodesbehavinglike examples
+  -- 493
 
   -- part 2
-
--- update
-
-update :: HasCallStack => W -> IO W
-update w = do
-  -- "Regardless of how the unit's turn ends, the next unit in the round takes its turn."
-  return $
-    w{ wtime = wtime w + 1
-      } -- & if (wunits w'==wunits w) then endworld "units have stabilised" else id
-
--- display. these return the unmodified World for easier chaining
-
-printworld :: W -> IO W
-printworld w@W{..} = do
-  -- printf "%d:  \n%s\n" wtime (ppShow wunits)
-  -- let (_,(xmax,ymax)) = A.bounds wmap
-  --     bgcs  = fmap showtile wmap
-  --     allcs = bgcs A.// [ ((ux u, uy u), showunit u) | u <- wunits]
-  -- mapM_ putStrLn [ [allcs A.! (x,y) | x <- [0..xmax]] | y <- [0..ymax] ]
-  putStrLn ""
-  return w
-
-printstats w@W{..} = do
-  when (wtime `mod` 1000 == 0) $ printf "%4d\n" wtime
-  return w
-
-printdots w@W{..} = do
-  when (wtime `mod` 1000 == 0) (putStr ".")
-  return w
-
-printsummary :: W -> Double -> IO ()
-printsummary W{..} t = do
-  -- let (_,(_,ymax)) = A.bounds wmap
-  -- setCursorPosition (ymax+3) 0
-  printf "\n%s in %dth tick\n" (fromMaybe "" wend) wtime
-  -- printf "%.3fs (%.0f ticks/s)\n" t wtime (fromIntegral wtime / t)
-
-initterm = do
-  hideCursor
-  hSetEcho stdout False
-  hSetBuffering stdin  NoBuffering
-  hSetBuffering stdout NoBuffering
-
-resetterm = do
-  setSGR [Reset]
-  showCursor
-
-toscreenx = (+1)
-toscreeny = (+2)
-
--- display in an ansi terminal and pause for the given number of seconds
--- (or if negative, wait and handle keypress)
-displayworld :: HasCallStack => Delay -> W -> IO W
-displayworld d w@W{..} = do
-  Just (Window{..}) <- size
-
-  setSGR [
-     SetColor Background Dull Black
-    ,SetColor Foreground Vivid White
-    ,SetConsoleIntensity BoldIntensity
-    ,SetSwapForegroundBackground False
-    ]
-  setCursorPosition 0 0
-  clearScreen
-  putStrLn $ "t " ++ show wtime ++ "  "
-
-  setSGR [
-     SetColor Background Dull Black
-    ,SetColor Foreground Dull Red
-    ,SetConsoleIntensity FaintIntensity
-    ,SetSwapForegroundBackground False
-    ]
-  -- let bg = fmap showtile wmap
-  --     (_,(xmax,ymax)) = A.bounds wmap
-  -- putStrLn $ " " ++ concatMap (take 1.reverse.show) [0..xmax]
-  -- let maprows   = [ lastdigit y : [bg A.! (x,y) | x <- [0..xmax]]
-  --                   | y <- [0..ymax] ]
-  --     unitstats = [ intercalate ", " $ map showunithp $ sortOn ux $ filter ((==y).uy) wunits
-  --                   | y <- [0..ymax] ]
-  -- forM_ (take (max (length maprows) (length unitstats)) $ zip
-  --         (maprows++repeat (replicate (xmax+1) ' '))
-  --         (unitstats++repeat "")) $
-  --   \(mr,us) -> putStrLn $ mr ++ "    " ++ us
-
-  setSGR [
-     SetColor Background Dull Black
-    ,SetColor Foreground Vivid White
-    ,SetConsoleIntensity BoldIntensity
-    ,SetSwapForegroundBackground False
-    ]
-  -- forM_ wunits $ \u@U{upos=(x,y),..} -> do
-  --   setCursorPosition (toscreeny y) (toscreenx x)
-  --   putChar $ showunit u
-
-  -- position cursor for debug printing
-  setCursorPosition (height-20) 0
-
-  pause w d
-  return w
-
--- wait for n seconds, or if n is negative, prompt for and handle a keypress
-pause :: W -> Delay -> IO ()
-pause w d = if d >=0 then delay d else void $ doinput w
-
--- wait for n seconds
-delay :: Seconds -> IO ()
-delay secs = threadDelay $ round $ secs * 1e6
-
--- prompt for and handle a keypress. Does not change the world.
-doinput :: W -> IO ()
-doinput w@W{..} = do
-  displayprompt
-  c <- getChar
-  case c of
-    'q' -> exitSuccess
-    -- 'i' -> displayinfo w (-1) "units" (ppShow wunits)
-    _   -> return ()
-  
-displayprompt = do
-  Just (Window{..}) <- size
-  setSGR [
-     SetColor Background Dull Black
-    ,SetColor Foreground Dull White
-    ,SetConsoleIntensity FaintIntensity
-    ,SetSwapForegroundBackground False
-    ]
-  setCursorPosition (height-4) 0
-  putStrLn $ "\n\nq: quit,  any other key: advance"
-
--- display a character with a style at some positions, and pause
-displaypoints :: W -> Delay -> Char -> [P] -> [SGR] -> IO W
-displaypoints w d c ps style = do
-  setSGR style
-  forM_ ps $ \(x,y) -> do
-    setCursorPosition (toscreeny y) (toscreenx x)
-    putChar c
-  pause w d
-  return w
-
-displayinfo :: W -> Delay -> String -> String -> IO ()
-displayinfo w@W{..} d label s = do
-  Just (Window{height,width}) <- size
-  -- let (_,(_,ymax)) = A.bounds wmap
-  setCursorPosition (height - 20) 0
-  putStrLn $ label ++ ":\n" ++ s
-  pause w d
-
-ttrace :: Show a => W -> String -> a -> a
-ttrace w msg x =
-  unsafePerformIO (displayinfo w 0 msg (ppShow x)) `seq`
-  x
-
+  let (opcodeassocs,[]) = identifyopcodes examples
+      opcode n = fromJust $ lookup n opcodeassocs
+      execu :: U -> M -> M
+      execu (U n a b c) = exec (I (opcode n) a b c)
+      regs1 = listArray (0,3) [0,0,0,0]
+      regs2 = foldl' (flip execu) regs1 instructions
+  pp $ regs2 ! 0
+  -- 445
