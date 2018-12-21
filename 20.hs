30e4ae2c4b2f196fafe376f0798bcbde53efb631
Simon Michael <simon@joyful.com>
2018-12-21 00:45:17 +0000

    20 part 1, handles examples but too slow for the real thing

--- 20.hs
+++ 20.hs
@@ -157,89 +157,266 @@ type P = (X,Y)
 data W = W { -- simulation world
    wtime    :: Ticks
   ,wend     :: Maybe String
+  ,wdirections    :: Directions
+  ,wrooms         :: [Room]
+  ,wbounds        :: ((X,Y),(X,Y))
+  ,wroompositions :: M.Map Room P
+  ,wpositionrooms :: M.Map P Room
+  ,wroomg         :: Graph
 } deriving (Eq,Show)
 
+-- ^EENWES$
+-- ^ENWWW(NEEE|SSE(EE|N))$
 type Directions = String
-
-parsedirections :: HasCallStack => String -> Directions
-parsedirections = drop 1 .init
 
 type Room = Int
 
--- from a string of directions, build
+type Path  = [P]  -- a path through point space, usually between significant points
+type Trail = [P]  -- a partial path, going nowhere special
+
+-- | A branch point, and (for most recent branch point only)
+-- any branch trails originating from it which have been parsed
+-- but not yet handled.
+type BranchInfo = (P,[Trail])
+
+move 'N' (x,y) = (x,y-1)
+move 'S' (x,y) = (x,y+1)
+move 'E' (x,y) = (x+1,y)
+move 'W' (x,y) = (x-1,y)
+
+-- | Extend this trail one step at the end in the given compass direction.
+-- An empty trail is extended from 0,0.
+extend :: Trail -> Char -> Trail
+extend [] d = [move d (0,0)]
+extend ps d = ps ++ [move d $ last ps]
+
+-- | Like extend, but only if a direction is provided.
+extend' :: Trail -> Maybe Char -> Trail
+extend' t Nothing  = t
+extend' t (Just c) = extend t c
+
+-- | Make a simple trail by following the given directions, containing no branches,
+-- from the starting point.
+maketrail :: P -> Directions -> Trail
+maketrail p ds = foldl' extend [p] ds
+            
+-- | Generate all possible trail fragments from the given directions,
+-- which may contain branches, starting from 0,0.
+-- The union of these trails' edges can be used to build a complete graph.
+trails :: Directions -> [Trail]
+trails ds  = trails' (0,0) [] ds
+
+-- | trails helper: follow the directions from the given point, exploring
+-- every branch of alternatives, generating all possible trail fragments.
+-- The end points of these trails will overlap so that the union of 
+-- their edges can be used to build a complete graph.
+--
+-- Keeps a stack of info regarding branches currently being parsed, most recent first.
+-- A '(' causes the most recent point to be pushed onto the stack as a branch point.
+-- On reaching '|', the trail just parsed since most recent branch point is also stored
+-- along with that branch point
+-- On reaching ')', the most recent branch info is popped, and any stored 
+-- branch trails, plus the final branch trail, are each extended by all remaining
+-- directions/alternatives. (Ie every '(' splits the world of trails,
+-- potentially lots of duplication/inefficiency.)
+--
+-- >>> mapM_ print $ trails' (0,0) [] "E(N|S)E"
+-- [(0,0),(1,0)]
+-- [(1,0),(1,-1)]
+-- [(1,0),(1,1)]
+-- [(1,-1),(2,-1)]
+-- [(1,1),(2,1)]
+-- 
+trails' :: P -> [BranchInfo] -> Directions -> [Trail]
+trails' _ _ "" = []
+trails' tstart branches ds =
+  -- trace ("trails': "++show (tstart,ds,branches)) () `seq`
+  let
+    (dsnext,drest)   = break (`elem` "(|)") ds
+    (d:drest')       = drest
+    (bpoint,btrails) = headDef (tstart,[]) branches
+    brest            = drop 1 branches
+    t                = maketrail bpoint dsnext
+    bts              = btrails ++ [t]
+    tend             = lastDef tstart t
+  in
+    if | null drest -> [t]
+       | d=='('     -> [t] ++ trails' tend ((tend,[]):branches) drest'
+       | d=='|'     -> trails' tstart ((bpoint, btrails++[t]):brest) drest'
+       | d==')'     -> bts ++ concat [trails' (last bt) (drop 1 branches) drest' | bt <- bts]
+-- XXX
+-- *Main> pp $ trails "E(N|E)E"
+-- [ [ ( 0 , 0 ) , ( 1 , 0 ) ]
+-- , [ ( 1 , 0 ) , ( 1 , -1 ) ]
+-- , [ ( 1 , 0 ) , ( 2 , 0 ) ]
+-- , [ ( 1 , -1 ) , ( 2 , -1 ) ]
+-- , [ ( 2 , 0 ) , ( 3 , 0 ) ]
+-- ]
+-- *Main> pp $ trails "(E(N|E)E)"
+-- [ [ ( 0 , 0 ) ]
+-- , [ ( 0 , 0 ) , ( 1 , 0 ) ]
+-- , [ ( 1 , 0 ) , ( 1 , -1 ) ]
+-- , [ ( 1 , 0 ) , ( 2 , 0 ) ]
+-- , [ ( 0 , 0 ) , ( 1 , 0 ) ]
+-- , [ ( 0 , 0 ) , ( 1 , 0 ) ]
+-- ]
+
+-- | From a directions string, build
 -- - a list of unique rooms in order visited
+-- - minimum and maximum x,y positions
 -- - a map from room to position
 -- - a map from position to room
 -- - a graph of room connections
-build :: String -> ([Room], M.Map Room P, M.Map P Room, Graph)
-build directions =
+build :: Directions -> ([Room], ((X,Y),(X,Y)), M.Map Room P, M.Map P Room, Graph)
+build ds =
   let
-    -- positions in order visited
-    psvisited = reverse $ foldl' follow [(0,0)] directions
+    -- trails generated, in sequence
+    ts =
+      -- ltrace "ts" $
+      trails ds
+    -- positions visited, in sequence, excluding duplicates
+    ps =
+      -- ltrace "psvisited" $
+      nub $ concat ts
+    -- position bounds
+    (xs,ys) = (map fst ps, map snd ps)
+    bounds = ((minimum xs, minimum ys), (maximum xs, maximum ys))
     -- assign room numbers to positions in order visited
-    roomsbyp = M.fromList $ reverse $ zip psvisited [0..]
+    positionrooms =
+      -- ltrace "positionrooms" $
+      M.fromList $ reverse $ zip ps [0..]
     -- map from room numbers to positions
-    roompositions = M.fromList $ map swap $ M.assocs roomsbyp
-    -- room numbers in order visited
-    roomvisits = map (fromJust . flip M.lookup roomsbyp) psvisited
+    roompositions = M.fromList $ map swap $ M.assocs positionrooms
+    -- unique room numbers in order visited (maybe with branches interleaved ?)
+    toroom = fromJust . flip M.lookup positionrooms
+    rooms =
+      -- ltrace "rooms" $
+      map toroom ps
     -- room connections, both ways
-    roomedges = concat [[(a,b), (b,a)] | (a,b) <- zip roomvisits (drop 1 roomvisits)]
-    -- unique room numbers in order visited
-    rooms = nub roomvisits
-    -- graph of rooms
-    roomg = buildG (minimum rooms, maximum rooms) roomedges
+    roomedges =
+      -- ltrace "roomedges" $
+      nub $ sort $ concat [
+        concat [[(a,b), (b,a)] | (a,b) <- zip rs (drop 1 rs)]
+        | ps <- ts, let rs = map toroom ps
+        ]
+    -- graph of room connections
+    roomg =
+      -- ltrace "roomg" $
+      buildG (minimum rooms, maximum rooms) roomedges
   in
-    (rooms, roompositions, roomsbyp, roomg)
-
-follow :: [P] -> Char -> [P]
-follow ps@((x,y):_) d = go d:ps
-  where
-    go 'N' = (x,y-1)
-    go 'S' = (x,y+1)
-    go 'E' = (x+1,y)
-    go 'W' = (x-1,y)
-
+    (rooms, bounds, roompositions, positionrooms, roomg)
 
 parse :: HasCallStack => String -> W
 parse s =
     W{wtime   = 0
      ,wend    = Nothing
-     -- ,wdirections = drop 1 $ init s
-    }
+     ,wdirections    = s
+     ,wrooms         = rooms
+     ,wbounds        = bounds
+     ,wroompositions = roompositions
+     ,wpositionrooms = positionrooms
+     ,wroomg         = roomg
+     }
+  where
+    (rooms, bounds, roompositions, positionrooms, roomg) = build s
+
+parsedirections :: HasCallStack => String -> Directions
+parsedirections = drop 1 .init
+
+gbounds g = (minimum vs, maximum vs) where vs = vertices g
+
+gdel :: Vertex -> Graph -> Graph
+gdel v g =
+  let
+    vs  = vertices g \\ [v]  -- v may get recreated, with no edges
+    b   = (minimum vs, maximum vs)
+    es' = [ (a,b) | (a,b) <- edges g, not $ v `elem` [a,b] ]
+  in
+    buildG b es'
+    
+edgesfrom g v = [(a,b) | (a,b) <- edges g, a==v]
+
+-- Longest path between start and end vertices in a graph.
+longestPath :: Graph -> Vertex -> Vertex -> [Edge]
+longestPath g start end
+  | start == end = []
+  | otherwise    = 
+    maximumBy (comparing length) $ ([]:)
+              [ e : path | e@(_,start') <- edgesfrom g start
+                         , let g' = gdel start g
+                         , let path = longestPath g' start' end ]
+
+-- Number of steps to the farthest room from room 0, and it's number and position.
+farthestRoom :: W -> (Int, Room, P)
+farthestRoom w@W{..} =
+  let
+    start = 0
+    ends = vertices wroomg \\ [start]
+    longestpath =
+      maximumBy (comparing length) [
+        traceWith ((("longest to "++show end++": ")++).show.length) $
+        longestPath wroomg start end | end <- ends ]
+    room = snd $ last longestpath
+  in
+    (length longestpath, room, fromJust $ M.lookup room wroompositions)
 
 -- main
 
 main :: HasCallStack => IO ()
 main = do
   -- part 1
--- What is the largest number of doors you would be required
--- to pass through to reach a room? That is, find the room for which
--- the shortest path from your starting location to that room would
--- require passing through the most doors; what is the fewest doors
--- you can pass through to reach it?
-  (t,w) <- timeItT $ -- bracket_ initterm resetterm $
-    iterateUntilM (isJust.wend)
-      (update >=> printworld) <=< printworld
-      -- (update >=> displayworld (-1)) <=< displayworld (-1)
-      $ parse e1
-  printsummary w t
+
+  -- To get a sense for the size of this facility, you'd like to determine
+  -- which room is furthest from you: specifically, you would like to find
+  -- the room for which the shortest path to that room would require
+  -- passing through the most doors.
+  
+  -- What is the largest number of doors you would be required
+  -- to pass through to reach a room? That is, find the room for which
+  -- the shortest path from your starting location to that room would
+  -- require passing through the most doors; what is the fewest doors
+  -- you can pass through to reach it?
+  
+  -- print $ farthestRoom $ parse "WNE"
+  -- print $ farthestRoom $ parse "ENWWW(NEEE|SSE(EE|N))"
+  -- print $ farthestRoom $ parse "ENNWSWW(NEWS|)SSSEEN(WNSE|)EE(SWEN|)NNN"
+  -- print $ farthestRoom $ parse "ESSWWN(E|NNENN(EESS(WNSE|)SSS|WWWSSSSE(SW|NNNE)))"
+  -- print $ farthestRoom $ parse "WSSEESWWWNW(S|NENNEEEENN(ESSSSW(NWSW|SSEN)|WSWWN(E|WWS(E|SS))))"
+  ds <- parsedirections . init <$> readFile "20.in"
+  timeIt $ print $ farthestRoom $ parse $ take 150 ds
+  -- timeIt $ print $ farthestRoom $ parse $ take 1000 ds
+  -- timeIt $ print $ farthestRoom $ parse $ take 2000 ds
+  -- timeIt $ print $ farthestRoom $ parse $ take 3000 ds
+  -- timeIt $ print $ farthestRoom $ parse $ take 4000 ds
+  -- timeIt $ print $ farthestRoom $ parse ds
+
+  -- pp w
+  -- printworld w
+  return ()
+
+  -- (t,w) <- timeItT $ -- bracket_ initterm resetterm $
+  --   iterateUntilM (isJust.wend)
+  --     (update >=> printworld) <=< printworld
+  --     -- (update >=> displayworld (-1)) <=< displayworld (-1)
+  --     $ parse e1
+  -- printsummary w t
 
 -- part 2
 
 -- update
 
--- trigger the end of the world, and set the reason unless it's already set
-worldend :: String -> W -> W
-worldend reason w@W{..} = w{wend=maybe (Just reason) Just wend}
+-- -- trigger the end of the world, and set the reason unless it's already set
+-- worldend :: String -> W -> W
+-- worldend reason w@W{..} = w{wend=maybe (Just reason) Just wend}
 
-update :: HasCallStack => W -> IO W
-update w = do
-  let w' = w
-  let w'' =
-        w'{
-         wtime = wtime w + 1
-        } & if (w'' == w) then worldend "world has stabilised" else id
-  return w''
+-- update :: HasCallStack => W -> IO W
+-- update w = do
+--   let w' = w
+--   let w'' =
+--         w'{
+--          wtime = wtime w + 1
+--         } & if (w'' == w) then worldend "world has stabilised" else id
+--   return w''
 
 -- display. these return the unmodified World for easier chaining
 
@@ -250,6 +427,14 @@ toscreeny = (+5)
 printworld :: W -> IO W
 printworld w@W{..} = do
   printf "\n%d:  \n" wtime
+  printrooms w
+
+printrooms w@W{..} = do
+  let ((xmin,ymin),(xmax,ymax)) = wbounds
+  let row y = '\n' : concat [maybe "   " (printf "%3d") $
+                              M.lookup (x,y) wpositionrooms | x <- [xmin..xmax]]
+  let rows = [row y | y <- [ymin..ymax]]
+  mapM_ putStrLn rows
   return w
 
 printsummary :: W -> Double -> IO ()
